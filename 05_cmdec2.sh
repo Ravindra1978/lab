bfea908230633dd1ea5bfb0ec9664f7d5583272f
Ravindra1978 <ravindrab44@gmail.com>
2023-11-25 13:22:25 +0530

    05_cmdec2.sh

--- 05_cmdec2.sh
+++ 05_cmdec2.sh
@@ -15,17 +15,16 @@ fi
 echo "creating $i instance"
 IP_ADDRESS=$(aws ec2 run-instances --image-id $IMAGE_ID  --instance-type $INSTANCE_TYPE  --security-group-ids $SECURITY_GROUP_ID --query 'Instances[0].PrivateIpAddress' --tag-specifications 'ResourceType=instance,Tags=[{Key=Name,Value=$i}]')
 echo "created $i instance: $IP_ADDRESS"
-
-    # aws route53 change-resource-record-sets --hosted-zone-id Z01163212KDRMFKEXYVZE --change-batch '
-    # {
-    #         "Changes": [{
-    #         "Action": "CREATE",
-    #                 "ResourceRecordSet": {
-    #                     "Name": "'$i.$DOMAIN_NAME'",
-    #                     "Type": "A",
-    #                         "TTL": 300,
-    #                         "ResourceRecords": [{ "Value": "'$IP_ADDRESS'"}]
-    #                     }}]
-    #         }
-    #         '
+    aws route53 change-resource-record-sets --hosted-zone-id Z01163212KDRMFKEXYVZE --change-batch '
+     {
+             "Changes": [{
+             "Action": "CREATE",
+                     "ResourceRecordSet": {
+                         "Name": "'$i.$DOMAIN_NAME'",
+                         "Type": "A",
+                         "TTL": 300,
+                         "ResourceRecords": [{ "Value": "'$IP_ADDRESS'"}]
+                         }}]
+             }
+             '
     done
