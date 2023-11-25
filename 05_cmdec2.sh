8145ce484e49a3f558633abc63bb1f9ca1d8687f
Ravindra1978 <ravindrab44@gmail.com>
2023-11-25 12:25:45 +0530

    adjusted route 53 code

--- 05_cmdec2.sh
+++ 05_cmdec2.sh
@@ -4,7 +4,7 @@ NAMES=("mongodb" "redis")
 IMAGE_ID=ami-03265a0778a880afb
 SECURITY_GROUP_ID=sg-02aff670556d3d4d0
 DOMAIN_NAME=ravidevops.cloud
-for i in ${NAMES[@]}
+for i in "${NAMES[@]}"
 do
  if [[ $i == "mongodb" || $i == "mysql" ]]
  then
@@ -12,27 +12,20 @@ do
 else
     INSTANCE_TYPE="t2.micro"
 fi
-    echo "creating $i instance"
-    IP_ADDRESS=$(aws ec2 run-instances --image-id $IMAGE_ID --instance-type $INSTANCE_TYPE  --security-group-ids $SECURITY_GROUP_ID --query 'Instances[0].PrivateIpAddress' --tag-specifications 'ResourceType=instance,Tags=[{Key=Name,Value="$i"}]')
-    echo "created $i instance: $IP_ADDRESS"
+echo "creating $i instance"
+IP_ADDRESS=$(aws ec2 run-instances --image-id $IMAGE_ID --instance-type $INSTANCE_TYPE  --security-group-ids $SECURITY_GROUP_ID --query 'Instances[0].PrivateIpAddress' --tag-specifications 'ResourceType=instance,Tags=[{Key=Name,Value="$i"}]')
+echo "created $i instance: $IP_ADDRESS"
 
-   aws route53 change-resource-record-sets --hosted-zone-id Z01163212KDRMFKEXYVZE  --change-batch '{
-        "Comment": "Create a new record set",
-        "Changes": [
-            {
-                "Action": "CREATE",
-                "ResourceRecordSet": {
-                    "Name": "'$i.$DOMAIN_NAME'",
-                    "Type": "A",
-                    "TTL": 300,
-                    "ResourceRecords": [
-                            {
-                                "Value": "'$IP_ADDRESS'"
-                            }
-                        ]
-                    }
-                }
-            ]
-        }'
-
+   aws route53 change-resource-record-sets --hosted-zone-id Z01163212KDRMFKEXYVZE --change-batch '
+   {
+        "Changes": [{
+        "Action": "CREATE",
+                   "ResourceRecordSet": {
+                      "Name": "'$i.$DOMAIN_NAME'",
+                       "Type": "A",
+                        "TTL": 300,
+                        "ResourceRecords": [{ "Value": "'$IP_ADDRESS'"}]
+                    }}]
+        }
+        '
     done
